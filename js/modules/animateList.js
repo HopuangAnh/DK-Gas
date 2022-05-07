c52a615dbdf456c19fd732c9675465016a40c72e
HopquangAnh <teoho37@gmail.com>
2022-05-07 21:33:24 +0700

    DKGAS

--- js/modules/animateList.js
+++ js/modules/animateList.js
@@ -1,17 +1,12 @@
 function AnimateList(){
-  var items = document.querySelectorAll('.active .product-market-list .item');
+  var items = document.querySelectorAll('.product-market-list .item');
   console.log(items)
   items.forEach((item,index) => {
     item.setAttribute('data-aos-delay',`${index*100}`)
-    // item.setAttribute('data-aos','fade-right')
+    item.setAttribute('data-aos','fade-right')
 
   })
-  // itemArr.forEach((item) => {
-  //   console.log(itemArr)
-  // })
 }
-
-// const animeList = 
 
 export { AnimateList };
 
