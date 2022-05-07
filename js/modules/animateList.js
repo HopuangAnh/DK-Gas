05ad69d102004b5c5e6814ca4dbeec583bfeaf25
HopquangAnh <teoho37@gmail.com>
2022-05-07 21:43:23 +0700

    Update 1

--- js/modules/animateList.js
+++ js/modules/animateList.js
@@ -1,6 +1,6 @@
 function AnimateList(){
   var items = document.querySelectorAll('.product-market-list .item');
-  console.log(items)
+  // console.log(items)
   items.forEach((item,index) => {
     item.setAttribute('data-aos-delay',`${index*100}`)
     item.setAttribute('data-aos','fade-right')
